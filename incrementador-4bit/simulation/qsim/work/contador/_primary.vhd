dddcf277524e59fd4c04edc94ab415b0c65c6a5b
pabloart702 <pabloarthur702@gmail.com>
2024-04-18 21:49:38 -0300

    Update

---
+++ incrementador-4bit/simulation/qsim/work/contador/_primary.vhd
@@ -0,0 +1,13 @@
+library verilog;
+use verilog.vl_types.all;
+entity contador is
+    port(
+        clk             : in     vl_logic;
+        ld              : in     vl_logic;
+        cnt             : in     vl_logic;
+        reset           : in     vl_logic;
+        data            : in     vl_logic_vector(3 downto 0);
+        tc              : out    vl_logic;
+        q               : out    vl_logic_vector(3 downto 0)
+    );
+end contador;
