e590c42155d10130db46b13167afbc1feff1dd63
MuriloBarros304 <murilobarros304@gmail.com>
2024-07-16 12:43:20 -0300

    calc

--- calculadora/calc.vhd
+++ calculadora/calc.vhd
@@ -6,31 +6,57 @@ entity calc is
 		a5, a4, a3, a2, a1, a0 : in bit;
 		b5, b4, b3, b2, b1, b0 : in bit;
 		clk : in bit;
+		sub : in bit;
+		load : in bit;
 		s5, s4, s3, s2, s1, s0 : out bit
 	);
 	
 component mux2x1_6b is
    port (
-      a : in std_logic_vector (5 downto 0);  -- Entrada a de 6 bits
-      b : in std_logic_vector (5 downto 0);  -- Entrada b de 6 bits
-      s : in bit;                   -- Sinal de seleção
-      d : out std_logic_vector (5 downto 0) -- Saída d de 6 bits
+      a : in bit_vector (5 downto 0);  
+      b : in bit_vector (5 downto 0);  
+      s : in bit;                  
+      d : out bit_vector (5 downto 0) 
     );
 end component;
 
-component somador is
+component somador_6b is
 	port(
-      a : in bit;      -- Primeiro operando de 1 bit
-      b : in bit;      -- Segundo operando de 1 bit
-      cin : in bit;    -- Carry-in de 1 bit
-      sum : out bit;   -- Soma de 1 bit
-      cout : out bit   -- Carry-out de 1 bit
+        a : in bit;    
+        b : in bit;      
+        cin : in bit;    
+        sum : out bit;   
+        cout : out bit   
 	);
 end component;	
 	
-signal s1 : bit;
-signal s2 : bit;
+component reg6 is
+	port(
+      c : in bit;  
+      load : in bit; 
+      i0, i1, i2, i3, i4, i5 : in bit; 
+      q0, q1, q2, q3, q4, q5 : out bit  
+	);
+end component;
+	
+signal sin : bit_vector (5 downto 0); -- entrada so somador
+signal carry1 : bit;
+signal carry2 : bit;
+signal carry3 : bit;
+signal carry4 : bit;
+signal rin0 : bit; -- entrada do registrador
+signal ld : bit;
 	
 architecture behaviorial of calc is
 begin
+	-- mux
+	m0: mux2x1_6b port map(a(0) => b0, b(0) => not(b0), s => sub, d(0) => sin(0)); -- verificar se pode usar not aqui
+	m1: mux2x1_6b port map(a(1) => b1, b(1) => not(b1), s => sub, d(1) => sin(1)); 
+	m2: mux2x1_6b port map(a(2) => b2, b(2) => not(b2), s => sub, d(2) => sin(2)); 
+	m3: mux2x1_6b port map(a(3) => b3, b(3) => not(b3), s => sub, d(3) => sin(3)); 
+	m4: mux2x1_6b port map(a(4) => b0, b(4) => not(b4), s => sub, d(4) => sin(4)); 
+	m5: mux2x1_6b port map(a(5) => b0, b(5) => not(b5), s => sub, d(5) => sin(5)); 
 	
+	-- somador
+	s0: somador port map(a => a0, b => sin(0), cin => carry1, cout => carry2, sum => rin0);
+	s1:
