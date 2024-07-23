a7bb4b95f28e21a19a3883ee4badfe19a3e1a174
MuriloBarros304 <murilobarros304@gmail.com>
2024-07-23 14:23:20 -0300

    adicionar mux

--- maq-vendas/regfile.vhd
+++ maq-vendas/regfile.vhd
@@ -3,39 +3,37 @@ entity flipflop is
     port (
         d : in bit;       -- entrada de dados
         clk : in bit;     -- sinal de clock
-        reset : in bit;   -- sinal de reset assíncrono
+        load : in bit;    -- sinal de habilitação
         q : out bit       -- saída do registrador
     );
 end flipflop;
 
 architecture behavioral of flipflop is -- comportamental
 begin
-    process(clk, reset)
+    process(clk, load)
     begin
-        if reset = '1' then
-            q <= '0';          
-        elsif (clk'event and clk = '1') then
+        if (clk'event and clk = '1' and load = '1') then
             q <= d;             
         end if;
     end process;
 end behavioral;
 
 -- registrador de 4 bits
-entity register is
+entity regist is
     port(
         d : in bit_vector(3 downto 0);  -- sinal de entrada
         clk : in bit;                   -- clock
-        reset : in bit;                 -- reset
-        q : out bit_vector(3 downto 0); -- sinal de saída
+        load : in bit;                  -- load
+        q : out bit_vector(3 downto 0)  -- sinal de saída
     );
-end register;
+end regist;
 
-architecture structural of register is
+architecture structural of regist is
     component flipflop is
         port(
             d : in bit;
             clk : in bit;
-            reset : in bit;
+            load : in bit;
             q : out bit
         );
     end component;
@@ -43,10 +41,10 @@ architecture structural of register is
     signal q_internal : bit_vector(3 downto 0);
 
 begin
-    ff0: flipflop port map(d => d(0), clk => clk, reset => reset, q => q_internal(0));
-    ff1: flipflop port map(d => d(1), clk => clk, reset => reset, q => q_internal(1));
-    ff2: flipflop port map(d => d(2), clk => clk, reset => reset, q => q_internal(2));
-    ff3: flipflop port map(d => d(3), clk => clk, reset => reset, q => q_internal(3));
+    ff0: flipflop port map(d => d(0), clk => clk, load => load, q => q_internal(0));
+    ff1: flipflop port map(d => d(1), clk => clk, load => load, q => q_internal(1));
+    ff2: flipflop port map(d => d(2), clk => clk, load => load, q => q_internal(2));
+    ff3: flipflop port map(d => d(3), clk => clk, load => load, q => q_internal(3));
 
     q <= q_internal;
 end structural;
@@ -94,22 +92,23 @@ end architecture;
 -- 3x8 para ler e escrever no array
 entity regfile is
     port(
-        w_data : in bit_vector(3 downto 0); -- dados para escrita
-        w_addr : in bit_vector(2 downto 0); -- endereço de escrita
-        w_en : bit;                         -- sinal de habilitação para escrita
-        r_data : in bit_vector(3 downto 0); -- dados para leitura
-        r_addr : in bit_vector(2 downto 0); -- endereço de leitura
-        r_en : bit;                         -- sinal de habilitação para leitura
+        w_data : in bit_vector(3 downto 0);  -- dados para escrita
+        w_addr : in bit_vector(2 downto 0);  -- endereço de escrita
+        w_en : in bit;                       -- sinal de habilitação para escrita
+        r_data : out bit_vector(3 downto 0); -- dados para leitura
+        r_addr : in bit_vector(2 downto 0);  -- endereço de leitura
+        r_en : in bit;                       -- sinal de habilitação para leitura
+        clock : in bit                       -- clock
     );
 end regfile;
 
 architecture registerfile of regfile is
-    component register is
+    component regist is
         port(
             d : in bit_vector(3 downto 0);  -- sinal de entrada
             clk : in bit;                   -- clock
-            reset : in bit;                 -- reset
-            q : out bit_vector(3 downto 0); -- sinal de saída
+            load : in bit;                  -- load
+            q : out bit_vector(3 downto 0)  -- sinal de saída
         );
     end component;
     
@@ -128,19 +127,34 @@ architecture registerfile of regfile is
             y : out bit_vector(3 downto 0) -- sinal de saída
         );
     end component;
-    signal d0_rf : bit_vector(7 downto 0); -- liga o decodificador de escrita aos registradores
-    signal ts_d1 : bit_vector(7 downto 0);   -- liga o three state driver ao decodificador de leitura
-    signal rf_ts : bit_vector(7 downto 0);   -- liga o banco de registradores aos three state drivers
+    signal d0_rf : bit_vector(7 downto 0); -- sinal de controle de escrita
+    signal ts_d1 : bit_vector(7 downto 0); -- sinal de controle de leitura
+    signal reg_out : bit_vector(3 downto 0); -- sinal de saída temporário para registro
+    signal r0_t0, r1_t1, r2_t2, r3_t3, r4_t4, r5_t5, r6_t6, r7_t7 : bit_vector(3 downto 0)
 begin
-    -- decodificadores
-    d0: decoder3x8 port map(a(0) => w_addr(0), a(1) => w_addr(1),a(2) => w_addr(2),
-    en => w_en, y(0) => d0_rf(0), y(1) => d0_rf(1), y(2) => d0_rf(2),
-    y(3) => d0_rf(3), y(4) => d0_rf(4), y(5) => d0_rf(5), y(6) => d0_rf(6),
-    y(7) => d0_rf(7));
+    -- decodificador de escrita
+    d0: decoder3x8 port map(a => w_addr, en => w_en, y => d0_rf);
 
-    d1: decoder3x8 port map(a(0) => r_addr(0), a(1) => r_addr(1),a(2) => r_addr(2),
-    en => r_en, y(0) => ts_d1(0), y(1) => ts_d1(1), y(2) => ts_d1(2), 
-    y(3) => ts_d1(3), y(4) => ts_d1(4), y(5) => ts_d1(5), y(6) => ts_d1(6), 
-    y(7) => ts_d1(7));
+    -- decodificador de leitura
+    d1: decoder3x8 port map(a => r_addr, en => r_en, y => ts_d1);
 
+    -- registradores
+    r0: regist port map(load => d0_rf(0), clk => clock, d => w_data, q => r0_t0);
+    r1: regist port map(load => d0_rf(1), clk => clock, d => w_data, q => r1_t1);
+    r2: regist port map(load => d0_rf(2), clk => clock, d => w_data, q => r2_t2);
+    r3: regist port map(load => d0_rf(3), clk => clock, d => w_data, q => r3_t3);
+    r4: regist port map(load => d0_rf(4), clk => clock, d => w_data, q => r4_t4);
+    r5: regist port map(load => d0_rf(5), clk => clock, d => w_data, q => r5_t5);
+    r6: regist port map(load => d0_rf(6), clk => clock, d => w_data, q => r6_t6);
+    r7: regist port map(load => d0_rf(7), clk => clock, d => w_data, q => r7_t7);
 
+    -- three state drivers
+    t0: threestate port map(a => r0_t0, s => ts_d1(0), y => r_data);
+    t1: threestate port map(a => r1_t1, s => ts_d1(1), y => r_data);
+    t2: threestate port map(a => r2_t2, s => ts_d1(2), y => r_data);
+    t3: threestate port map(a => r3_t3, s => ts_d1(3), y => r_data);
+    t4: threestate port map(a => r4_t4, s => ts_d1(4), y => r_data);
+    t5: threestate port map(a => r5_t5, s => ts_d1(5), y => r_data);
+    t6: threestate port map(a => r6_t6, s => ts_d1(6), y => r_data);
+    t7: threestate port map(a => r7_t7, s => ts_d1(7), y => r_data);
+end architecture;
--- maq-vendas/vend.vhd
+++ maq-vendas/vend.vhd
@@ -4,9 +4,16 @@ use ieee.std_logic_1164.all;
 
 entity vend is
     port(
-        a, b : in bit_vector (3 downto 0);
-        d : out bit_vector (3 downto 0);
-        out_eq, out_gt : out bit
+        --a, b : in bit_vector (3 downto 0);
+        --d : out bit_vector (3 downto 0);
+        --out_eq, out_gt : out bit
+		  w_data : in bit_vector(3 downto 0); -- dados para escrita
+        w_addr : in bit_vector(2 downto 0); -- endereço de escrita
+        w_en : in bit;                         -- sinal de habilitação para escrita
+        r_data : out bit_vector(3 downto 0); -- dados para leitura
+        r_addr : in bit_vector(2 downto 0); -- endereço de leitura
+        r_en : in bit;                         -- sinal de habilitação para leitura
+        clock : in bit                     -- clock
     );
 end vend;
 
@@ -24,7 +31,21 @@ architecture behav of vend is
             out_eq, out_gt : out bit
         );
     end component;
+	 
+	 component regfile is
+	     port(
+				w_data : in bit_vector(3 downto 0); -- dados para escrita
+				w_addr : in bit_vector(2 downto 0); -- endereço de escrita
+				w_en : in bit;                         -- sinal de habilitação para escrita
+				r_data : out bit_vector(3 downto 0); -- dados para leitura
+				r_addr : in bit_vector(2 downto 0); -- endereço de leitura
+				r_en : in bit;                         -- sinal de habilitação para leitura
+				clock : in bit                      -- clock				
+		  );
+	 end component;
 begin 
-    u0: subtractor port map(x => a, y => b, d => d);
-    u1: comparator port map(a => a, b => b, out_gt => out_gt, out_eq => out_eq);
+    --u0: subtractor port map(x => a, y => b, d => d);
+    --u1: comparator port map(a => a, b => b, out_gt => out_gt, out_eq => out_eq);
+	 u2: regfile port map(w_data => w_data, w_addr => w_addr, w_en => w_en, r_data => r_data,
+	 r_addr => r_addr, r_en => r_en, clock => clock);
 end behav;
